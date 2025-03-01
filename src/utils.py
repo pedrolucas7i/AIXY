79d77291028c383726e2ce6585ef4675fe63de9e
pedrolucas7i <pedrolucas.core7i@gmail.com>
2025-03-01 18:30:00 +0000

    Creation of brain and llm methods files and better stucturation

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ from time import sleep
 import sqlite3
 import logging
 import json
+import llm
 import env
 
 
@@ -48,6 +49,7 @@ def accessMemory(thing, category):
         
     else:
         logging.warning("Invalid Category!")
+        return None
 
 
 def addMemory(thing, definition, category):
@@ -58,33 +60,19 @@ def addMemory(thing, definition, category):
     conn.close()
 
 
-def makePrompt(model, toDo, thing, color=None, localization=None, MemoryFile=True):
-    logging.info("Making Prompt")
+def findObjectVisionPrompt(thing, localization=None, MemoryFile=True):
     definition = None
-    
-    if toDo is 'find':
-        category = 'visual'
-    elif toDo is 'search':
-        category = 'info'
-    else:
-        logging.warning("Invalid toDo! Changing category to 'info'...")
-        print("Invalid toDo! Changing toDo to 'search' and category to 'info'...")
-        toDo = 'search'
-        category = 'info'
-    
+
     if MemoryFile:
-        definition = accessMemory(thing, category)
-    
+        definition = accessMemory(thing, 'visual')
+        
     if definition is None:
-        if category is 'info':
-            data = {'thing': thing, "color": color, "localization": localization}
-            return env.OLLAMA_EN_SEARCH_PROMPT + f'{json.dumps(data)}'
-        elif category is 'visual':
-            data = f"Your main objective is to detect the '{thing}' object and approach it, when you find it you must come face to face with it, with a distance between you of 100mm, in this case you should ignore all other outputs and only provide the word 'finded', without additional explanations."
-            if color is not None and localization is not None:
-                data = f"Your main objective is to detect the '{thing}' object with the '{color}' color in the '{localization}' localization and approach it, when you find it you must come face to face with it, with a distance between you of 100mm, in this case you should ignore all other outputs and only provide the word 'finded', without additional explanations."
-            elif color is not None and localization is None:
-                data = f"Your main objective is to detect the '{thing}' object with the '{color}' color and approach it, when you find it you must come face to face with it, with a distance between you of 100mm, in this case you should ignore all other outputs and only provide the word 'finded', without additional explanations."
-            elif color is None and localization is not None:
-                data = f"Your main objective is to detect the '{thing}' object in the '{localization}' localization and approach it, when you find it you must come face to face with it, with a distance between you of 100mm, in this case you should ignore all other outputs and only provide the word 'finded', without additional explanations."
-            return env.OLLAMA_VISION_DECISION_PROMPT + data
+        definition = llm.get(env.OLLAMA_LANGUAGE_MODEL, env.OLLAMA_EN_SEARCH_VISUAL_PROMPT + thing)
+        addMemory(thing, definition, 'visual')
+    
+    prompt = f"Your main objective is to detect the '{thing}' object and approach it, when you find it you must come face to face with it, with a distance between you of 100mm, in this case you should ignore all other outputs and only provide the word 'finded', without additional explanations. Visual description: {definition}"
+    
+    if localization is not None:
+        prompt = f"Your main objective is to detect the '{thing}' object in the '{localization}' localization and approach it, when you find it you must come face to face with it, with a distance between you of 100mm, in this case you should ignore all other outputs and only provide the word 'finded', without additional explanations. Visual description: {definition}"
+        
+    return env.OLLAMA_VISION_DECISION_PROMPT + prompt
--- src/vision.py
+++ src/vision.py
@@ -2,30 +2,23 @@ from ollama import Client
 from time import sleep
 import logging
 import camera
+import utils
+import llm
 import env
 
 # Configure logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+        
+def decide(waitTime=0.02):
+    for frame in camera.captured_images():
+        decision = llm.get(env.OLLAMA_LANGUAGE_MODEL, env.OLLAMA_VISION_DECISION_PROMPT, frame)
+        logging.info(f"Decided: {decision}")
+        yield decision
+        sleep(waitTime)
 
-def askOllama(model, image_stream, prompt):
-    client = Client(host=env.OLLAMA_HOST)
-    try:
-        return client.generate(model, prompt, image_stream)
-    except Exception as e:
-        logging.error(f"An error occurred in askOllama: {str(e)}")
-        
-def decide(waitTime):
+def find(thing, localization=None, waitTime=0.02):
     for frame in camera.captured_images():
-        decision = askOllama(env.OLLAMA_LANGUAGE_MODEL, frame, env.OLLAMA_VISION_DECISION_PROMPT)
+        decision = llm.get(env.OLLAMA_LANGUAGE_MODEL, utils.findObjectVisionPrompt(thing, localization), frame)
         logging.info(f"Decided: {decision}")
         yield decision
         sleep(waitTime)
-        
-# Waiting to make find method, for real object search
-def find(thing, waitTime):
-    for frame in camera.captured_images():
-        decision = askOllama(env.OLLAMA_LANGUAGE_MODEL, frame, env.OLLAMA_VISION_DECISION_PROMPT)
-        logging.info(f"Decided: {decision}")
-        yield decision
-        sleep(waitTime)
-        
