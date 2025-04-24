019b27544581552c93d551c6af1471ef04769454
pedrolucas7i <pedrolucas.core7i@gmail.com>
2025-04-24 20:48:06 +0100

    Fixed to the newer voice

--- src/tts.py
+++ src/tts.py
@@ -8,8 +8,8 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 
 # Initialize TTS engine globally
 logging.info("Initializing TTS")
-tts = pyttsx3.init("mb/mb-us2")
-tts.setProperty('rate', tts.getProperty('rate') - 20)
+engine = pyttsx3.init(driverName='espeak')
+engine.setProperty('rate', engine.getProperty('rate') - 20)
 
 def speech(text):
     logging.info(f"Converting message to speech: {message}")
@@ -18,16 +18,10 @@ def speech(text):
     # Define the speech function that uses the initialized TTS engine
     def play_speech():
         try:
-            logging.info("Converting message to speech")
-            
-            # Adjust the speech rate (optional)
-            rate = tts.getProperty('rate')
-            
-            # Add a short delay before converting message to speech
-            time.sleep(0.5)  # Adjust the delay as needed
-            
-            tts.say(text)
-            tts.runAndWait()  # Wait for speech playback to complete
+            logging.info("Converting message to speech")  
+            engine.setProperty('voice', 'mb-us2')
+            engine.say(text)
+            engine.runAndWait()  # Wait for speech playback to complete
             logging.info("Speech playback completed")
         except Exception as e:
             logging.error(f"An error occurred during speech playback: {str(e)}")
