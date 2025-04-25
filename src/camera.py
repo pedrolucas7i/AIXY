86e43a90e6ea77198526dd8919548a3f2ef35e6a
pedrolucas7i <pedrolucas.core7i@gmail.com>
2025-04-25 14:02:38 +0100

    Turn the image 180º because of the contruction of robot

--- src/camera.py
+++ src/camera.py
@@ -9,7 +9,9 @@ def captured_images():
     with Picamera2() as picam2:
         picam2.configure(picam2.create_still_configuration(main={'size': (512, 384)}))
         picam2.start()
-        return convertToBytes(picam2.capture_array())
+        frame = picam2.capture_array()
+        rotated = np.rot90(frame, 2)
+        return convertToBytes(rotated)
         
 def convertToBytes(image_array):
     """Convert NumPy image array to bytes."""
@@ -18,8 +20,5 @@ def convertToBytes(image_array):
 
 def getWebStream():
     while True:
-        with Picamera2() as picam2:
-            picam2.configure(picam2.create_still_configuration(main={'size': (512, 384)}))
-            picam2.start()
-            yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + convertToBytes(picam2.capture_array()) + b'\r\n')
+        yield (b'--frame\r\n'   
+                b'Content-Type: image/jpeg\r\n\r\n' + captured_images() + b'\r\n')
